1071235176fb286503ae36211ffafdd4f1453352
Nikolas2610 <npsillou@gmail.com>
2022-10-09 01:11:36 +0300

    OMCG-14 Add Navbar and Hero Page

---
+++ src/views/auth/LoginPage.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        This is a Login Page
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#4E9F3D',
+        'secondary': '#1E5128',
+        'dark': '#191A19',
+        'light': '#D8E9A8',
+      }
+    },
   },
   plugins: [],
 }
