390e1a9315a5207d7e352b9b61f97567874ff17e
Nikolas2610 <npsillou@gmail.com>
2022-10-09 02:00:21 +0300

    OMCG-16 Create Login form

--- src/views/auth/LoginPage.vue
+++ src/views/auth/LoginPage.vue
@@ -1,5 +1,54 @@
 <template>
+    <div class="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-dark">
+        <div class="container py-20">
+            <div class="sm:mx-auto sm:w-full sm:max-w-lg">
+                <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-white">Sign in to our<br><span
+                        class="text-primary">Creative Gave</span></h2>
+            </div>
+            <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+                <div class="bg-primary py-8 px-4 shadow sm:rounded-lg sm:px-10">
+                    <form class="space-y-6" action="#" method="POST">
 
+                        <div>
+                            <label for="email" class="block text-sm font-medium text-white">Email address</label>
+                            <div class="mt-1">
+                                <input id="email" name="email" type="email"
+                                    class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-secondary focus:outline-none sm:text-sm" />
+                            </div>
+                        </div>
+
+                        <div>
+                            <label for="password" class="block text-sm font-medium text-white">Password</label>
+                            <div class="mt-1">
+                                <input id="password" name="password" type="password"
+                                    class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-secondary focus:outline-none sm:text-sm" />
+                            </div>
+                        </div>
+
+                        <div class="flex items-center justify-between">
+                            <div class="flex items-center group">
+                                <input id="remember-me" name="remember-me" type="checkbox"
+                                    class="h-4 w-4 rounded border-black text-black focus:ring-primary" />
+                                <label for="remember-me" class="ml-2 block text-sm text-white group-hover:text-secondary">Remember me</label>
+                            </div>
+
+                            <div class="text-sm">
+                                <a href="#" class="font-medium text-white hover:text-secondary">Forgot your
+                                    password?</a>
+                            </div>
+                        </div>
+
+                        <div>
+                            <button type="submit"
+                                class="mt-14 flex w-full justify-center rounded-md border border-transparent bg-dark py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-secondary">
+                                Login
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
