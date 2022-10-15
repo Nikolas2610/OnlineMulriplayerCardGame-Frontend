abc77d15524a9c42ca1af2920e2bf56329f70d71
Nikolas2610 <npsillou@gmail.com>
2022-10-15 19:34:48 +0300

    OMCG-22 Forgot Password API validation | Create Reset Password Form | Add reset password API

---
+++ src/views/auth/email-confirmation/ForgotPasswordValidationPage.vue
@@ -0,0 +1,78 @@
+<template>
+    <section class="bg-dark" v-if="state.loading">
+        <div class="container py-40">
+            <div>
+                <!-- Email not exists | Wrong Token -->
+                <TokenNotExists v-if="state.message === 2" />
+
+                <!-- Token Expire -->
+                <TokenExpireMessage v-if="state.message === 3" />
+
+                <!-- Server error -->
+                <ServerErrorMessage v-if="state.message === 4" />
+            </div>
+        </div>
+    </section>
+    <div class="container" v-else>
+        <PreLoader />
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue';
+import { useUserStore } from '@/stores/UserStore'
+import ServerErrorMessage from './messages/ServerErrorMessage.vue'
+import TokenExpireMessage from './messages/TokenExpireMessage.vue'
+import TokenNotExists from './messages/TokenNotExists.vue'
+import PreLoader from '@/components/PreLoader.vue'
+import router from '@/router';
+
+const userStore = useUserStore();
+const state = ref({ message: 0, loading: false });
+
+onMounted(async () => {
+    // Get token from URL
+    const token = getToken();
+    if (!token) {
+        // Redirect to 404 page
+        router.push({ name: '404' });
+        return
+    }
+    // Verify user with token and return response
+    const response = await userStore.forgotPasswordVerify(token);
+    // If response success redirect user to reset the password
+    if (response === 'success') {
+        router.push({ name: 'reset-password' });
+    }
+    // Check response message to print to the user the correct message
+    selectNotification(response);
+    // Load the page
+    state.value.loading = true;
+});
+
+// Select correct message for the user
+const selectNotification = (response: string): void => {
+    switch (response) {
+        case 'Bad confirmation token':
+        case 'Bad Request':
+            state.value.message = 2
+            break;
+        case 'Forgot password token expired':
+            state.value.message = 3
+            break;
+        case 'Internal server error':
+            state.value.message = 4
+            break;
+        default:
+            state.value.message = 2
+            break;
+    }
+}
+
+// Get token from the URL
+const getToken = (): string | null => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    return urlParams.get('token');
+}
+</script>
