be3801e09c245ce96fcaaf528b0f6808ddbf4d8d
Nikolas2610 <npsillou@gmail.com>
2023-04-21 17:37:44 +0300

    Go production

--- dist/assets/PreLoader.075b38d4.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as s,c as t,h as o}from"./index.34df172d.js";const c="/assets/loader.ba67f5cb.gif",r={},n={class:"flex items-center justify-center",style:{height:"93.3vh"}},a=o("img",{src:c,alt:"loading"},null,-1),_=[a];function i(l,d){return s(),t("div",n,_)}const h=e(r,[["render",i]]);export{h as P};
--- dist/assets/cards.5dbcf4c1.js
+++
@@ -1,1 +0,0 @@
-const s="/assets/cards.fface172.svg";export{s as _};
